fbe4db2ce9a61a070601d5a79cd90df9715a02bd
DesignC <znehraks@gmail.com>
2020-01-15 01:41:27 +0900

    MessageNavi & AuthScreen start

---
+++ navigation/MessageNavigation.js
@@ -0,0 +1,8 @@
+import { createStackNavigator } from "react-navigation-stack";
+import Message from "../screens/Messages/Message";
+import Messages from "../screens/Messages/Message";
+
+export default createStackNavigator({
+  Messages,
+  Message
+});
--- navigation/TabNavigation.js
+++ navigation/TabNavigation.js
@@ -1,14 +1,33 @@
-import { View } from "react-native";
-import { createAppContainer } from "react-navigation";
+import React from "react";
+import { createStackNavigator } from "react-navigation-stack";
 import { createBottomTabNavigator } from "react-navigation-tabs";
 import Home from "../screens/Home";
 import Search from "../screens/Search";
 import Notifications from "../screens/Notifications";
 import Profile from "../screens/Profile";
+import MessageLink from "../components/MessageLink";
+import { View } from "react-native";
+
+const stackFactory = (initialRoute, customConfig) =>
+  createStackNavigator({
+    initialRoute: {
+      screen: initialRoute,
+      navigationOptions: { ...customConfig }
+    }
+  });
 
 export default createBottomTabNavigator({
-  Home,
-  Search,
+  Home: {
+    screen: stackFactory(Home, {
+      title: "Home",
+      headerRight: <MessageLink />
+    })
+  },
+  Search: {
+    screen: stackFactory(Search, {
+      title: "Search"
+    })
+  },
   Add: {
     screen: View,
     navigationOptions: {
@@ -17,6 +36,14 @@ export default createBottomTabNavigator({
       }
     }
   },
-  Notifications,
-  Profile
+  Notifications: {
+    screen: stackFactory(Notifications, {
+      title: "Notifications"
+    })
+  },
+  Profile: {
+    screen: stackFactory(Profile, {
+      title: "Profile"
+    })
+  }
 });
